f40a108cf97c20be35ce49490037a2a5470ba63d
pedroCollogno <pierre.collignon@yahoo.fr>
2019-09-16 16:25:08 +0200

    Comments and datetime

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -2,9 +2,19 @@ import requests
 import json
 from datetime import datetime
 
-res = requests.get('https://www.oui.sncf/calendar/cdp/api/proposals/v3/outward/FRPAR/FRMZM/2019-09-20/12-HAPPY_CARD/2/fr/fr?extendedToLocality=true&onlyDirectTrains=true')
+required_day = '2019-09-20' # Required format
+departure_city = 'PAR' # See other station's code
+arrival_city = 'MZM'
+
+options = 'extendedToLocality=true&onlyDirectTrains=true' # Don't care, don't touch
+res = requests.get('https://www.oui.sncf/calendar/cdp/api/proposals/v3/outward/FR' + departure_city +'/FR' + arrival_city + '/' + required_day + '/12-HAPPY_CARD/2/fr/fr?' + options)
 
 jsoned_data = json.loads(res.content)
 
 for billet in jsoned_data:
-    print(str(int(billet['totalPrice']) / 100) + '€ Départ : ' + str(billet['departureDate']).split('T')[1] + ' Arrivée : ' + str(billet['arrivalDate']).split('T')[1])
+
+    # Useful infos : billet['totalPrice'] (in centimes), billet['departureDate'] and billet['arrivalDate'] (only care about the time, discard the rest)
+    departure_time = datetime.strptime(str(billet['departureDate']), "%Y-%m-%dT%H:%M")
+    arrival_time = datetime.strptime(str(billet['arrivalDate']), "%Y-%m-%dT%H:%M")
+
+    print(str(int(billet['totalPrice']) / 100) + '€ Départ : ' + departure_time.strftime("%Hh%M") + ' Arrivée : ' + arrival_time.strftime("%Hh%M"))
