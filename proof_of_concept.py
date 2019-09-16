256f42314bce43a37ffea353fe2aff385d53fc14
Mokopower <marin.merlin@me.com>
2019-09-17 00:35:55 +0200

    First commit, the point is to monitor each trip in a different thread

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,10 +1,20 @@
 import requests
 import json
 from datetime import datetime
+import threading
 
-res = requests.get('https://www.oui.sncf/calendar/cdp/api/proposals/v3/outward/FRPAR/FRMZM/2019-09-20/12-HAPPY_CARD/2/fr/fr?extendedToLocality=true&onlyDirectTrains=true')
+with open filename as f:
+    content = f.readlines()
 
-jsoned_data = json.loads(res.content)
+class travelThread(threading.Thread):
+    def _init_(self,date,origin,destination):
+        self.date = date
+        self.origin = origin
+        self.destination = destination
 
-for billet in jsoned_data:
-    print(str(int(billet['totalPrice']) / 100) + '€ Départ : ' + str(billet['departureDate']).split('T')[1] + ' Arrivée : ' + str(billet['arrivalDate']).split('T')[1])
+    def run():
+        res = requests.get('https://www.oui.sncf/calendar/cdp/api/proposals/v3/outward/FRPAR/FRMZM/2019-09-20/12-HAPPY_CARD/2/fr/fr?extendedToLocality=true&onlyDirectTrains=true')
+        jsoned_data = json.loads(res.content)
+
+        for billet in jsoned_data:
+            print(str(int(billet['totalPrice']) / 100) + '€ Départ : ' + str(billet['departureDate']).split('T')[1] + ' Arrivée : ' + str(billet['arrivalDate']).split('T')[1])
