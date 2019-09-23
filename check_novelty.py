f88303025013d06744c667a8660d2d9a58420733
pedroCollogno <pierre.collignon@yahoo.fr>
2019-09-23 18:35:02 +0200

    Code made PEP-8 compatible

--- check_novelty.py
+++ check_novelty.py
@@ -1,13 +1,13 @@
 import json
-from notifyUser import notifyUser
+from notify_user import notify_user
 
-def checkNovelty(old_ids,dic_list,origin,destination,date,user_list):
+def check_novelty(old_ids, dic_list, origin, destination, date, user_list):
     new_ids = []
     for dic in dic_list:
         if dic['is_tgvmax']:
             if not dic['id'] in old_ids:
                 print("New train detected, Départ : " + dic['departure_time'].strftime("%Hh%M"))
-                notifyUser(origin,destination,date,dic,user_list)
+                notify_user(origin, destination, date, dic, user_list)
         new_ids.append(dic['id'])
     return new_ids
             
