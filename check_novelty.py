d8783eb3d8c56b2ba4ed47c48c1cb5c03f9ad4f1
Marin Merlin <marin.merlin@me.com>
2019-11-15 09:40:00 +0100

    Updated check_novelty, doesnt trigger notification on first iteration

--- check_novelty.py
+++ check_novelty.py
@@ -1,13 +1,14 @@
 import json
 from notify_user import notify_user
 
-def check_novelty(old_ids, dic_list, origin, destination, date, user_list):
+def check_novelty(old_ids, dic_list, origin, destination, date, user_list, iteration):
     new_ids = []
     for dic in dic_list:
         if dic['is_tgvmax']:
             if not dic['id'] in old_ids:
                 print("New train detected, Départ : " + dic['departure_time'].strftime("%Hh%M"))
-                notify_user(origin, destination, date, dic, user_list)
+                if(iteration > 0):
+                    notify_user(origin, destination, date, dic, user_list)
         new_ids.append(dic['id'])
     return new_ids
             
