e1b93e6a4b3c1c754d7e2ff341ea71a707ebcbaa
Mokopower <marin.merlin@me.com>
2019-11-22 19:09:34 +0100

    Created main and strucutre for the js server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const http = require('http');
-const callSendAPI = require('./messenger_api');
-
+const checkNewTrains = require('./functions/checkNewTrains');
+const callAPI = require('./functions/callAPI')
+const notifyUser = require('./functions/notifyUser');
+const updateTrainsInTrip = require('./functions/updateTrainsInTrip');
 const server = http.createServer((req, res) => {
     res.end('This is my server response!');
 });
-console.log("labite on port ",process.env.PORT)
-callSendAPI('1907097259403451', 'la bite');
+
+main = () => {
+    while(true){
+        getAllTripsFromDB().then((trips)=> {
+            _.forEach(trips,(trip)=>{
+                trains = callAPI(trip);
+                new_trains = checkNewTrains(trip,trains);
+                _.forEach(new_trains,(new_train)=>{
+                    notifyUser(trip,new_train);
+                    updateTrainsInTrip(trip,train);
+                })
+            })
+        })
+        sleep();
+    }
+}
+
+main();
+
 server.listen(process.env.PORT || 3000);
 
 
